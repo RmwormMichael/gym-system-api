15dd6c1f7c3a0dd00448f8a5a69838086bd5e069
Michael <rmworm18@gmail.com>
2025-12-29 10:43:41 -0500

    listo para sistema de pago

--- src/routes/plan.routes.js
+++ src/routes/plan.routes.js
@@ -3,7 +3,7 @@ const auth = require('../middlewares/auth.middleware');
 const planCtrl = require('../controllers/plan.controller');
 
 router.post('/', auth, planCtrl.crearPlan);
-router.get('/', auth, planCtrl.obtenerPlanes);
+router.get('/', planCtrl.obtenerPlanes);
 router.get('/:id', auth, planCtrl.obtenerPlan);
 router.put('/:id', auth, planCtrl.actualizarPlan);
 router.delete('/:id', auth, planCtrl.eliminarPlan);
